cc28748395776e4c80a46b12f4b2d8dc4cedf8f3
Diego Oliveira Teixeira <diego.ot@discente.ufma.br>
2025-07-23 15:35:53 -0300

    Display sudoku

---
+++ Playgrond.py
@@ -0,0 +1,26 @@
+# janela.blit(numero_1, (0, 0))
+#     x = 0
+#     for i in range(quantidade):
+#         janela.blit(numero_1, (posicao_x[i], posicao_y[i])
+
+
+# if (teclas[pygame.K_UP] or teclas[pygame.K_w]) and pos_y_player > 1:
+#     pos_y_player -= vel_nave_player
+#
+# if (teclas[pygame.K_DOWN] or teclas[pygame.K_s]) and pos_y_player < 440:
+#     pos_y_player += vel_nave_player
+#
+# if (teclas[pygame.K_LEFT] or teclas[pygame.K_a]) and pos_x_player > 1:
+#     pos_x_player -= vel_nave_player
+#
+# if (teclas[pygame.K_RIGHT] or teclas[pygame.K_d]) and pos_x_player < 870:
+#     pos_x_player += vel_nave_player
+
+
+# nave_player = pygame.image.load(
+#     "C:/Users/diego/PycharmProjects/PythonProject/Jogos Pygame/Jogo de Aprendizado/Assets/sprite_nave_pequena.png")
+# nave_player = pygame.transform.scale(nave_player, (30, 30))
+
+
+# janela.blit(nave_player, (pos_x_player, pos_y_player))
+# print(pos_x_player, pos_y_player)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import Matrizes
+pygame.init()
 pygame.display.set_caption('Sudoku')
 
 x, y = 850, 850
@@ -7,6 +9,15 @@ janela = pygame.display.set_mode([x, y])
 pos_x_player = 430
 pos_y_player = 400
 vel_nave_player = 10
+
+quantidade = 3
+
+posicao_x = [150, 420, 690]
+posicao_y = [240, 510, 780]
+
+
+
+
 
 
 
@@ -18,34 +29,75 @@ while loop:
 
     teclas = pygame.key.get_pressed()
 
-    if (teclas[pygame.K_UP] or teclas[pygame.K_w]) and pos_y_player > 1:
-        pos_y_player -= vel_nave_player
-
-    if (teclas[pygame.K_DOWN] or teclas[pygame.K_s]) and pos_y_player < 440:
-        pos_y_player += vel_nave_player
-
-    if (teclas[pygame.K_LEFT] or teclas[pygame.K_a]) and pos_x_player > 1:
-        pos_x_player -= vel_nave_player
-
-    if (teclas[pygame.K_RIGHT] or teclas[pygame.K_d]) and pos_x_player < 870:
-        pos_x_player += vel_nave_player
 
 
 
     imagem_fundo = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Tabuleiro.png")
-    nave_player = pygame.image.load("C:/Users/diego/PycharmProjects/PythonProject/Jogos Pygame/Jogo de Aprendizado/Assets/sprite_nave_pequena.png")
-    nave_player = pygame.transform.scale(nave_player, (30, 30))
-    numero_um = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_1-Photoroom.jpg")
-    numero_um = pygame.transform.scale(numero_um, (55, 55))
-
-
-
+    numero_1 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_1-Photoroom.jpg")
+    numero_1 = pygame.transform.scale(numero_1, (55, 55))
+    numero_2 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_2-Photoroom.jpg")
+    numero_2 = pygame.transform.scale(numero_2, (55, 55))
+    numero_3 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_3-Photoroom.jpg")
+    numero_3 = pygame.transform.scale(numero_3, (55, 55))
+    numero_4 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_4-Photoroom.jpg")
+    numero_4 = pygame.transform.scale(numero_4, (55, 55))
+    numero_5 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_5-Photoroom.jpg")
+    numero_5 = pygame.transform.scale(numero_5, (55, 55))
+    numero_6 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_6-Photoroom.jpg")
+    numero_6 = pygame.transform.scale(numero_6, (55, 55))
+    numero_7 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_7-Photoroom.jpg")
+    numero_7 = pygame.transform.scale(numero_7, (55, 55))
+    numero_8 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_8-Photoroom.jpg")
+    numero_8 = pygame.transform.scale(numero_8, (55, 55))
+    numero_9 = pygame.image.load("C:/Users/diego/OneDrive/Área de Trabalho/-/Pygame/Sudoku-Pygame/Assets/Numero_9-Photoroom.jpg")
+    numero_9 = pygame.transform.scale(numero_9, (55, 55))
 
     janela.blit(imagem_fundo, (0, 0))
-    janela.blit(numero_um, (420, 510))
 
-    janela.blit(nave_player, (pos_x_player, pos_y_player))
-    print(pos_x_player, pos_y_player)
+
+
+
+
+    janela.blit(numero_1, (215, 240))
+
+
+    y = 0
+    for i in range(len(Matrizes.Sudoku_playground)):
+        x = 0
+        for j in range(len(Matrizes.Sudoku_playground)):
+            if Matrizes.Sudoku_playground[i][j] == 1:
+                janela.blit(numero_1, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 2:
+                janela.blit(numero_2, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 3:
+                janela.blit(numero_3, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 4:
+                janela.blit(numero_4, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 5:
+                janela.blit(numero_5, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 6:
+                janela.blit(numero_6, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 7:
+                janela.blit(numero_7, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 8:
+                janela.blit(numero_8, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+            elif Matrizes.Sudoku_playground[i][j] == 9:
+                janela.blit(numero_9, (Matrizes.posicoes_x[x], Matrizes.posicoes_y[y]))
+
+
+            x += 1
+        y += 1
+
+
+
+
+
+
+
+
+
+
+
 
 
     pygame.display.update()
