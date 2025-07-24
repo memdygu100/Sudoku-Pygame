daa15d35c0728c704b8b5b737a8365b71e903c26
Diego Oliveira Teixeira <diego.ot@discente.ufma.br>
2025-07-24 15:18:54 -0300

    Mouse detection resumido

--- main.py
+++ main.py
@@ -24,51 +24,65 @@ while loop:
             loop = False
 
         if pressionado:
-            if (posicao_click_x > 140 and posicao_click_y > 230) and (posicao_click_x < 210 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][0] = 1
-                        pressionado = False
-            if (posicao_click_x > 210 and posicao_click_y > 230) and (posicao_click_x < 270 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][1] = 1
-                        pressionado = False
-            if (posicao_click_x > 270 and posicao_click_y > 230) and (posicao_click_x < 340 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][2] = 1
-                        pressionado = False
-            if (posicao_click_x > 340 and posicao_click_y > 230) and (posicao_click_x < 410 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][3] = 1
-                        pressionado = False
-            if (posicao_click_x > 410 and posicao_click_y > 230) and (posicao_click_x < 480 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][4] = 1
-                        pressionado = False
-            if (posicao_click_x > 480 and posicao_click_y > 230) and (posicao_click_x < 550 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][5] = 1
-                        pressionado = False
-            if (posicao_click_x > 550 and posicao_click_y > 230) and (posicao_click_x < 620 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][6] = 1
-                        pressionado = False
-            if (posicao_click_x > 610 and posicao_click_y > 230) and (posicao_click_x < 680 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][7] = 1
-                        pressionado = False
-            if (posicao_click_x > 680 and posicao_click_y > 230) and (posicao_click_x < 750 and posicao_click_y < 300):
-                if events.type == pygame.KEYDOWN:
-                    if events.key == pygame.K_1:
-                        Matrizes.Sudoku_playground[0][8] = 1
-                        pressionado = False
+            if posicao_click_y > 230 and posicao_click_y < 300:
+
+                if posicao_click_x > 140 and posicao_click_x < 210:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][0] = 1
+                            pressionado = False
+                if posicao_click_x > 210 and posicao_click_x < 270:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][1] = 1
+                            pressionado = False
+                if posicao_click_x > 270 and posicao_click_x < 340:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][2] = 1
+                            pressionado = False
+                if posicao_click_x > 340 and posicao_click_x < 410:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][3] = 1
+                            pressionado = False
+                if posicao_click_x > 410 and posicao_click_x < 480:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][4] = 1
+                            pressionado = False
+                if posicao_click_x > 480 and posicao_click_x < 550:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][5] = 1
+                            pressionado = False
+                if posicao_click_x > 550 and posicao_click_x < 620:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][6] = 1
+                            pressionado = False
+                if posicao_click_x > 620 and posicao_click_x < 690:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][7] = 1
+                            pressionado = False
+                if posicao_click_x > 690 and posicao_click_x < 760:
+                    if events.type == pygame.KEYDOWN:
+                        if events.key == pygame.K_1:
+                            Matrizes.Sudoku_playground[0][8] = 1
+                            pressionado = False
+
+
+
+
+
+
+
+
+
+
+
+
 
     teclas = pygame.key.get_pressed()
 
